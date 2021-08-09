39124e97bce148818e18a70251b420c3379a5e0b
Quinn T <Quinn.GitHub@gmail.com>
2021-08-08 19:49:32 -0500

    Upgrade to using CloudProxy to bypass cloudflare.

--- wallpapers/scraping.go
+++ wallpapers/scraping.go
@@ -1,10 +1,13 @@
 package wallpapers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"regexp"
+	"strings"
 )
 
 var ErrUnrecognisedWallpaperRepoType = fmt.Errorf("unrecognised wallpaper repo type")
@@ -20,34 +23,100 @@ func GetFiles(repoType WallpaperRepoType, Url string) ([]string, error) {
 	}
 }
 
-// Wraps http.NewRequest (with GET and no body) passed to http.DefaultClient.Do with user agent set to a browser's
-func httpGetWithFakeUserAgent(Url string) (resp *http.Response, err error) {
-	req, err := http.NewRequest("GET", Url, nil)
-	if err != nil {
-		return nil, err
-	}
-	// req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
-	return http.DefaultClient.Do(req)
+// Solution to a cloudproxy request
+type CloudProxySolution struct {
+	// URL requested
+	URL string `json:"url"`
+	// HTTP status from request
+	Status int `json:"status"`
+	// Headers in response
+	Headers map[string]string `json:"headers"`
+	// Raw text response of page
+	Response string `json:"response"`
+	// Our user-agent
+	Useragent string `json:"userAgent"`
+	// Cookies returned
+	Cookies []Cookie `json:"cookies"`
 }
 
-func getEyyIndexerFiles(Url string) (urls []string, err error) {
-	findFiles := regexp.MustCompile(`<td data-raw="image" class="download"><a href="([^"]*)"`)
-	resp, err := httpGetWithFakeUserAgent(Url)
+type CloudProxyResponse struct {
+	Solution *CloudProxySolution `json:"solution"`
+	// "ok" or "error"
+	Status string `json:"status"`
+	// Error message
+	Message        string `json:"message"`
+	Starttimestamp int64  `json:"startTimestamp"`
+	Endtimestamp   int64  `json:"endTimestamp"`
+	// CloudProxy version
+	Version string `json:"version"`
+}
+
+// A browser cookie
+type Cookie struct {
+	Name     string  `json:"name"`
+	Value    string  `json:"value"`
+	Domain   string  `json:"domain"`
+	Path     string  `json:"path"`
+	Expires  float64 `json:"expires"`
+	Size     int     `json:"size"`
+	Httponly bool    `json:"httpOnly"`
+	Secure   bool    `json:"secure"`
+	Session  bool    `json:"session"`
+	Samesite string  `json:"sameSite"`
+}
+
+// Cookies created in cloudproxy sessions
+var CloudProxyCookies []Cookie
+
+// Uses cloudproxy (if availible on local port 8191) to get the content of a website.
+// Does not solve captchas, only bypasses CloudFlare IUAM.
+//
+// CloudProxy: https://github.com/NoahCardoza/CloudProxy
+func CloudProxyGetContent(Url string) (content *CloudProxyResponse, err error) {
+	req, err := http.NewRequest("POST", "http://localhost:8191/v1",
+		strings.NewReader(fmt.Sprintf(`{
+			"cmd": "request.get",
+			"url":"%s",
+			"userAgent": "%s",
+			"maxTimeout": 20000
+		  }`, Url, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"),
+		))
 	if err != nil {
 		return nil, err
 	}
-
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
-	body := string(bytes)
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		fmt.Println(body)
-		panic(fmt.Errorf("status code is %v; body is in stdout", resp.StatusCode))
+	err = json.Unmarshal(bytes, &content)
+	if err != nil {
+		return nil, err
 	}
-	matches := findFiles.FindAllStringSubmatch(body, -1)
-	baseURL := "https://" + resp.Request.URL.Host
+	if content.Status != "ok" {
+		return nil, fmt.Errorf("cloudproxy: %s", content.Message)
+	}
+	return content, nil
+}
+
+func getEyyIndexerFiles(Url string) (urls []string, err error) {
+	findFiles := regexp.MustCompile(`<td data-raw="image" class="download"><a href="([^"]*)"`)
+
+	content, err := CloudProxyGetContent(Url)
+	if err != nil {
+		return nil, err
+	}
+
+	parsed, err := url.Parse(content.Solution.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	matches := findFiles.FindAllStringSubmatch(content.Solution.Response, -1)
+	baseURL := "https://" + parsed.Host
 	for _, match := range matches {
 		urls = append(urls, baseURL+match[1])
 	}
